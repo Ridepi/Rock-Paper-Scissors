0700747de0d22f29cd014b9b1bc5b3dfb468586e
Rianne <rianne.visser@outlook.com>
2022-11-16 20:16:24 +0100

    Add function PlayRound

--- script.js
+++ script.js
@@ -15,3 +15,15 @@ console.log(computerSelection);
 //ask player for input, make it case insensitive
 let playerSelection = prompt("Rock, paper or scissors?").toLowerCase();
 console.log (playerSelection);
+
+//function to play a round of Rock Paper Scissors -- Add tie option!
+function PlayRound(playerSelection,computerSelection) {
+    if ((playerSelection === "rock" && computerSelection ==="scissors") || (playerSelection === "paper" && computerSelection === "rock") || (playerSelection === "scissors" && computerSelection === "paper")) {
+        return `You win! ${playerSelection} beats ${computerSelection}!`
+    } else if (playerSelection === computerSelection) {
+        return "It's a tie!"
+    } else {
+        return `You lose! ${computerSelection} beats ${playerSelection}!`
+    }
+}
+console.log (PlayRound(playerSelection,computerSelection));
