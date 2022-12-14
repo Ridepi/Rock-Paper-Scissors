184ffec2af7c855ef9becb304cd5267387a5c5da
Rianne <rianne.visser@outlook.com>
2022-12-14 21:48:56 +0100

    Link button to script, link computer selection to img to display

--- script.js
+++ script.js
@@ -1,15 +1,41 @@
+//get the computer's choice
 function getComputerChoice () {
     let options = ["rock", "paper", "scissors"];
     let computerChoice = options[Math.floor(Math.random()*options.length)];
     return computerChoice
 }
 
-function getPlayerChoice() {
-    return prompt("Rock, paper or scissors?").toLowerCase();
-}
-
+let computerChoiceImg = document.createElement("img");
 let personScore = 0;
 let computerScore = 0;
+let playerSelection;
+
+//select the player's choice, generate the computer's selection and play a round
+document.getElementById("playerChoiceRock").addEventListener("click", function () {
+    playerSelection = "rock";
+    onClick();
+});
+document.getElementById("playerChoicePaper").addEventListener("click", function() {
+    playerSelection = "paper";
+    onClick();
+});
+document.getElementById("playerChoiceScissors").addEventListener("click", function() {
+    playerSelection = "scissors";
+    onClick();
+});
+
+function onClick (){
+    let computerSelection = getComputerChoice();
+    if (computerSelection === "rock") {
+        computerChoiceImg.src="img/rock.jpg";
+    } else if (computerSelection === "paper") {
+        computerChoiceImg.src = "img/paper.jpg";
+    } else {
+        computerChoiceImg.src = "img/scissors.jpg";
+    }
+    document.getElementById("computerChoicePicture").appendChild(computerChoiceImg);
+    PlayRound(playerSelection, computerSelection)
+}
 
 function PlayRound(playerSelection,computerSelection) {
     if ((playerSelection === "rock" && computerSelection ==="scissors") || 
@@ -23,7 +49,26 @@ function PlayRound(playerSelection,computerSelection) {
         personScore += 0; 
         computerScore += 1;
     }
+    console.log(playerSelection,personScore);
+    console.log(computerSelection,computerScore);
 }
+
+
+/*function PlayRound(playerSelection,computerSelection) {
+    if ((playerSelection === "rock" && computerSelection ==="scissors") || 
+        (playerSelection === "paper" && computerSelection === "rock") || 
+        (playerSelection === "scissors" && computerSelection === "paper")) {
+        personScore += 1; 
+        computerScore +=0;
+    } else if (playerSelection === computerSelection) {
+
+    } else {
+        personScore += 0; 
+        computerScore += 1;
+    }
+    console.log(personscore);
+    console.log(computerScore);
+}*/
 
 function game() {
     let i = 0;
